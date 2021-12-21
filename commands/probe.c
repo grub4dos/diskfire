dfefe8b08717e50a287c5eb113676c7fde82fe20
A1ive <10670106+a1ive@users.noreply.github.com>
2021-12-21 17:08:15 +0800

    probe list

--- commands/probe.c
+++ commands/probe.c
@@ -20,7 +20,7 @@ static grub_err_t probe_partmap(grub_disk_t disk)
 	grub_partition_map_t partmap;
 	if (disk->partition && disk->partition->partmap->name)
 	{
-		grub_printf("%s\n", disk->partition->partmap->name);
+		grub_printf("%s", disk->partition->partmap->name);
 		return GRUB_ERR_NONE;
 	}
 	parent = grub_disk_open(disk->name);
@@ -30,12 +30,12 @@ static grub_err_t probe_partmap(grub_disk_t disk)
 	{
 		if (partmap->iterate(parent, probe_partmap_hook, NULL) == GRUB_ERR_NONE)
 		{
-			grub_printf("%s\n", partmap->name);
+			grub_printf("%s", partmap->name);
 			goto out;
 		}
 		grub_errno = GRUB_ERR_NONE;
 	}
-	grub_printf("none\n");
+	grub_printf("none");
 out:
 	grub_errno = GRUB_ERR_NONE;
 	grub_disk_close(parent);
@@ -47,7 +47,7 @@ static grub_err_t probe_fs(grub_disk_t disk)
 	grub_fs_t fs;
 	fs = grub_fs_probe(disk);
 	if (fs && fs->name)
-		grub_printf("%s\n", fs->name);
+		grub_printf("%s", fs->name);
 	return GRUB_ERR_NONE;
 }
 
@@ -62,7 +62,7 @@ static grub_err_t probe_fsuuid(grub_disk_t disk)
 		if (grub_errno == GRUB_ERR_NONE && uuid)
 		{
 			if (grub_strlen(uuid))
-				grub_printf("%s\n", uuid);
+				grub_printf("%s", uuid);
 			grub_free(uuid);
 			grub_errno = GRUB_ERR_NONE;
 		}
@@ -81,7 +81,7 @@ static grub_err_t probe_label(grub_disk_t disk)
 		if (grub_errno == GRUB_ERR_NONE && label)
 		{
 			if (grub_strlen(label))
-				grub_printf("%s\n", label);
+				grub_printf("%s", label);
 			grub_free(label);
 			grub_errno = GRUB_ERR_NONE;
 		}
@@ -91,13 +91,13 @@ static grub_err_t probe_label(grub_disk_t disk)
 
 static grub_err_t probe_size(grub_disk_t disk)
 {
-	grub_printf("%llu\n", grub_disk_native_sectors(disk) << GRUB_DISK_SECTOR_BITS);
+	grub_printf("%llu", grub_disk_native_sectors(disk) << GRUB_DISK_SECTOR_BITS);
 	return GRUB_ERR_NONE;
 }
 
 static grub_err_t probe_startlba(grub_disk_t disk)
 {
-	grub_printf("%llu\n", disk->partition ? grub_partition_get_start(disk->partition) : 0);
+	grub_printf("%llu", disk->partition ? grub_partition_get_start(disk->partition) : 0);
 	return GRUB_ERR_NONE;
 }
 
@@ -120,17 +120,44 @@ parse_probe_opt(const char* arg, grub_disk_t disk)
 	return grub_error(GRUB_ERR_BAD_ARGUMENT, "invalid option %s\n", arg);
 }
 
+static int
+list_disk_iter(const char* name, void* data)
+{
+	const char* arg = data;
+	grub_disk_t disk = 0;
+	disk = grub_disk_open(name);
+	if (arg && disk)
+	{
+		grub_printf("%s ", name);
+		parse_probe_opt(arg, disk);
+		grub_printf("\n");
+	}
+	if (disk)
+		grub_disk_close(disk);
+	grub_errno = GRUB_ERR_NONE;
+	return 0;
+}
+
+static grub_err_t
+list_disk(char* arg)
+{
+	grub_disk_iterate(list_disk_iter, arg);
+	return GRUB_ERR_NONE;
+}
+
 static grub_err_t
 cmd_probe(struct grub_command* cmd, int argc, char* argv[])
 {
 	(void)cmd;
 	char* ptr;
 	grub_disk_t disk = 0;
-	if (argc < 2)
+	if (argc < 1)
 	{
 		grub_error(GRUB_ERR_BAD_ARGUMENT, "missing arguments");
 		goto fail;
 	}
+	if (argc == 1)
+		return list_disk(argv[0]);
 	ptr = argv[1] + grub_strlen(argv[1]) - 1;
 	if (argv[1][0] == '(' && *ptr == ')')
 	{
@@ -143,6 +170,7 @@ cmd_probe(struct grub_command* cmd, int argc, char* argv[])
 	if (!disk)
 		goto fail;
 	parse_probe_opt(argv[0], disk);
+	grub_printf("\n");
 fail:
 	if (disk)
 		grub_disk_close(disk);
@@ -154,7 +182,7 @@ fail:
 static void
 help_probe(struct grub_command* cmd)
 {
-	grub_printf("%s OPTIONS DISK\n", cmd->name);
+	grub_printf("%s OPTIONS [DISK]\n", cmd->name);
 	grub_printf("Retrieve disk info.\nOPTIONS:\n");
 	grub_printf("  --partmap   Determine partition map type.\n");
 	grub_printf("  --fs        Determine filesystem type.\n");
