b5d644c24b8b8067cb5609948f9db077b1628a6e
A1ive <10670106+a1ive@users.noreply.github.com>
2021-12-31 08:58:14 +0800

    add grub2 copyright

--- io/lzopio.c
+++ io/lzopio.c
@@ -1,7 +1,7 @@
 /* lzopio.c - decompression support for lzop */
 /*
  *  GRUB  --  GRand Unified Bootloader
- *  Copyright (C) 2011  Free Software Foundation, Inc.
+ *  Copyright (C) 2022  Free Software Foundation, Inc.
  *
  *  GRUB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
